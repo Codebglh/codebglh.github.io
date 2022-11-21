72dc7b66a61df70e66cda2c31efbb530aa1b04e4
bgcode <447701537@qq.com>
2022-11-21 22:04:52 +0800

    bgcode

--- js/video.js
+++ js/video.js
@@ -1,14 +1,14 @@
 function jiekou() {
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    var xmlhttp = new XMLHttpRequest(); //创建一个xml请求
+    xmlhttp.onreadystatechange = function() { //当XML请求成功时调用匿名函数
         if (this.readyState == 4 && this.status == 200) {
             myArr = JSON.parse(this.responseText);
-            for (var i = 0; i < myArr.jiekou.length; i++) {
+            for (var i = 0; i < myArr.jiekou.length; i++) { //循环遍历json文件
                 var s = myArr.jiekou[i].url; //链接
-                var q = myArr.jiekou[i].name;
-                var para = create1("option", s, q)
-                var element = getid("jk");
-                element.appendChild(para);
+                var q = myArr.jiekou[i].name; //名字
+                var para = create1("option", s, q) //创建一个option标签并赋值
+                var element = getid("jk"); //获取标签
+                element.appendChild(para); //写入
             }
         }
     };
@@ -17,19 +17,19 @@ function jiekou() {
 }
 
 function getid(name) {
-    return document.getElementById(name)
+    return document.getElementById(name) //获取标签
 }
 
 function create(lable_1, value_1) {
-    var a = document.createElement(lable_1);
-    a.id = value_1;
+    var a = document.createElement(lable_1); //创建标签
+    a.id = value_1; //赋值
     return a;
 }
 
 function create1(name_1, value_1, value_2) {
     var a = document.createElement(name_1);
-    a.value = value_1;
-    a.innerHTML = value_2;
+    a.value = value_1; //创建标签
+    a.innerHTML = value_2; //赋值
     return a
 }
 
@@ -37,10 +37,10 @@ function jiexi(url) {
     var jxApi = getid("jk"); //获取选择按钮
     var jxurl = getid("jk").selectedIndex; //获取选中的
     var jkv = jxApi.options[jxurl].value; //获取选择接口链接
-    getid("iframe_a").src = jkv + url
+    getid("iframe_a").src = jkv + url //拼接并发送
 }
 
-function bofang(url) {
+function bofang(url) { //创建一个dplayer
     const dp = new DPlayer({
         container: getid('dplayer'),
         video: {
@@ -58,12 +58,12 @@ function bofang(url) {
     });
 }
 
-function delete_table(name) {
-    if (getid(name)) {
+function delete_table(name) { //删除标签
+    if (getid(name)) { //获取id如果能获取则删除
         var a = getid("box")
         var para = getid(name);
         a.removeChild(para);
-    } else {
+    } else { //不能则返回
         return;
     }
 
@@ -72,9 +72,9 @@ function delete_table(name) {
 
 function main() {
     let url = getid("url_1").value
-    var ss = RegExp(/m3u8/g)
+    var ss = RegExp(/m3u8/g) //正则
     var hh = getid("box");
-    if (url.match(ss)) {
+    if (url.match(ss)) { //匹配正则
         //执行m3u8格式的程序
         delete_table("iframe_a")
         if (getid("dplayer")) {
